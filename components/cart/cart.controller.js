c0b7bb9ba5afcf1439846245b34c25ff7bd6ce5f
Nikolo <voropn@gmail.com>
2019-09-29 23:10:39 +0300

    Initial dist subtree commit

--- components/cart/cart.controller.js
+++
@@ -1,9 +0,0 @@
-import { CartModel } from "./cart.model";
-import { CartView } from "./cart.view";
-
-export class CartController {
-  constructor() {
-    this.cartModel = new CartModel();
-    this.cartView = new CartView();
-  }
-}
--- src/main.js
+++
@@ -1,8 +0,0 @@
-import '../node_modules/skeleton-scss/scss/skeleton.scss';
-import './main.sass';
-import main from './main.html';
-import { Router } from './router';
-import { Translator } from './translator';
-
-new Translator({template: main, output: document.body}).render();
-new Router();
